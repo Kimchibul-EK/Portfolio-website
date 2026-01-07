1c047be6ebd090a4703359333c0b7d406744511a
Kimchibul <kiul0001@stud.ek.dk>
2026-01-07 06:20:44 +0100

    nav

---
+++ javascript/navmenu.js
@@ -0,0 +1,15 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
+s;
