11363434c23c2e88853e33413a4118b9c0000eea
Kimchibul <kiul0001@stud.ek.dk>
2026-02-05 14:56:34 +0900

    media block og youtube embeds virker

--- javascript/script.js
+++ javascript/script.js
@@ -75,3 +75,16 @@ document.addEventListener("DOMContentLoaded", () => {
     root.style.colorScheme = event.target.value;
   });
 });
+
+/*------------------------------*/
+/* Image Carousel               */
+/*------------------------------*/
+
+const images = document.querySelectorAll(".carousel-item");
+let current = 0;
+
+setInterval(() => {
+  images[current].classList.remove("active");
+  current = (current + 1) % images.length;
+  images[current].classList.add("active");
+}, 7000);
